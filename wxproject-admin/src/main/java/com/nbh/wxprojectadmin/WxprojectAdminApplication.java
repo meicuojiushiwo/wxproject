77c56ff3078fae0fddb17673ef2f9700162751aa
nbh <1606966314@qq.com>
2019-01-21 23:33:32 +0800

    完善缓存配置，swagger配置，startrt配置

---
+++ wxproject-admin/src/main/java/com/nbh/wxprojectadmin/WxprojectAdminApplication.java
@@ -0,0 +1,14 @@
+package com.nbh.wxprojectadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxprojectAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxprojectAdminApplication.class, args);
+    }
+
+}
+
